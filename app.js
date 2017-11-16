d69f0938ed42a28e0c2a959f63a58569196711e9
Ryunhee Han <ryuneeee@gmail.com>
2017-11-17 06:30:31 +0900

    1. Add bot router 2. change webhook URI

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-const config = {
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.CHANNEL_SECRET
-};
-
 //express
 const express = require('express');
 const app = express();
 
-//line bot & vm
-const line = require('@line/bot-sdk');
-const client = new line.Client(config);
-const vm = require('vm');
-const timeout = 1000;
-
-//line bot webhook
-app.post('/webhook', line.middleware(config), (req, res) => {
-    res.json(req.body.events.map(handleEvent))
-});
+//express router
+app.use('/', require('./routes/index'));
+app.use('/bot', require('./routes/bot'));
+app.use('/users', require('./routes/users'));
 
 // parser & view engine setup
 const path = require('path');
@@ -31,10 +20,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-//express router
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,68 +38,5 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
-
-// toString overriding for prevent that user can seeing.
-Function.prototype.toString = function(){
-    return "You can't see function code."
-};
-
-// require module wrapper
-const whiteRequire = function(moduleName){
-    let whitelist = ['http'];
-    if(whitelist.includes(moduleName)) {
-        return require(moduleName);
-    }else{
-        throw new Error("You can't load unauthorized script.");
-    }
-};
-
-const contexts = new Map();
-
-const getCtxId = function(e) {
-    if (e.source.type === 'user')  return e.source.userId;
-    if (e.source.type === 'group') return e.source.groupId;
-    if (e.source.type === 'room')  return e.source.roomId;
-};
-
-const getContext = function(id) {
-    if (!contexts.has(id)) {
-        contexts.set(id, new Map());
-    }
-    return contexts.get(id);
-};
-
-function handleEvent(event) {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        return Promise.resolve(null);
-    }
-    let ctxId = getCtxId(event);
-    let context = getContext(ctxId);
-    const reply = function (message) {
-        client.pushMessage(ctxId, {type: 'text', 'text': message});
-    };
-
-    if (event.message.text[0] === '>') {
-        let code = event.message.text.substring(1);
-
-        code = '(function(){\n' + code + '\n})();';
-        const sandbox = {
-            require: whiteRequire,
-            client: client, // TODO: will be deleted
-            event: event,
-            message: event.message,
-            reply: reply,
-            setValue: function(k, v) { context.set(k, v); },
-            getValue: function(k) { return context.get(k); },
-            delValue: function(k) { context.delete(k); }
-        };
-        try {
-            const script = new vm.Script(code);
-            script.runInNewContext(sandbox, {timeout: timeout, displayErrors: true});
-        } catch (e) {
-            client.replyMessage(event.replyToken, { type: 'text', 'text': e.message});
-        }
-    }
-}
 
 module.exports = app;
