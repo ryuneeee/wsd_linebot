706a86994f18a29747a802029c17decfd8f56007
win32virus <win32virus@naver.com>
2017-11-27 05:31:19 +0900

    add navbar, login, join modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar.component';
+import { LoginModalComponent } from './login.modal.component';
+import { JoinModalComponent } from "./join.modal.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+      LoginModalComponent,
+      JoinModalComponent,
+      NavbarComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
