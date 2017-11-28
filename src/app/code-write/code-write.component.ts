869a0fc59e7b15236c497845969b71e634ab0a07
Park Jung-hwan <AhnMo@users.noreply.github.com>
2017-11-29 04:10:32 +0900

    write_compoenent

--- src/app/code-write/code-write.component.ts
+++ src/app/code-write/code-write.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CodeService } from '../code.service';
+import { Code } from '../code';
+import { Result } from '../result';
 
 @Component({
   selector: 'app-code-write',
   templateUrl: './code-write.component.html',
-  styleUrls: ['./code-write.component.css']
+  styleUrls: ['./code-write.component.css'],
+  providers:  [ CodeService ]
 })
-export class CodeWriteComponent implements OnInit {
+export class CodeWriteComponent implements OnInit, OnDestroy {
+  private sub: any;
+  code: Code;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private service: CodeService) { }
 
   ngOnInit() {
+    this.code = new Code();
+    this.sub = this.route.params.subscribe(params => {
+       this.code.ctxId = params['id'];
+    });
+  }
+
+  submit() {
+    let _id = this.code.ctxId;
+    this.service.createCode(this.code).subscribe((m: Result) => {
+      if (m.result == 'success') {
+        alert("Done!");
+        this.router.navigateByUrl('/list/' + _id);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
