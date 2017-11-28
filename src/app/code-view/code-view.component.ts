0d14b81e8ee9f4f63559073b84e4ac53634e3870
Park Jung-hwan <AhnMo@users.noreply.github.com>
2017-11-29 04:10:57 +0900

    view component

--- src/app/code-view/code-view.component.ts
+++ src/app/code-view/code-view.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CodeService } from '../code.service';
+import { Code } from '../code';
+import { Result } from '../result';
 
 @Component({
   selector: 'app-code-view',
   templateUrl: './code-view.component.html',
-  styleUrls: ['./code-view.component.css']
+  styleUrls: ['./code-view.component.css'],
+  providers:  [ CodeService ]
 })
-export class CodeViewComponent implements OnInit {
+export class CodeViewComponent implements OnInit, OnDestroy {
+  code: Code;
+  private sub: any;
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private router: Router,
+    private route: ActivatedRoute,
+    private service: CodeService) { }
 
   ngOnInit() {
+    this.code = new Code(); // before init...
+    this.sub = this.route.params.subscribe(params => {
+       this.getCode(params['id']);
+    });
+  }
+
+  getCode(id) {
+    this.service.getCode(id).subscribe((c: Code) => {
+      this.code = c;
+      this.code.id = id;
+    });
+  }
+
+  back() {
+    this.location.back();
+  }
+
+  edit() {
+    this.router.navigateByUrl('/edit/' + this.code.id);
+  }
+
+  delete() {
+    let _id = this.code.ctxId;
+    this.service.deleteCode(this.code).subscribe((r: Result) => {
+      if (r.result == 'success') {
+        alert('Done!');
+        this.router.navigateByUrl('/list/' + _id);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
