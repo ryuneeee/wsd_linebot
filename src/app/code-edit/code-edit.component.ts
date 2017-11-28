e786a28cf2182c2aaef52eb8b44e4d35110b82d0
Park Jung-hwan <AhnMo@users.noreply.github.com>
2017-11-29 04:13:08 +0900

    code edit component

--- src/app/code-edit/code-edit.component.ts
+++ src/app/code-edit/code-edit.component.ts
@@ -1,16 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CodeService } from '../code.service';
+import { Code } from '../code';
+import { Result } from '../result';
 
 @Component({
   selector: 'app-code-edit',
   templateUrl: './code-edit.component.html',
-  styleUrls: ['./code-edit.component.css']
+  styleUrls: ['./code-edit.component.css'],
+  providers:  [ CodeService ]
 })
-export class CodeEditComponent implements OnInit {
-  code: string = "let test = 'a';\nreply('foobar');\n\n";
+export class CodeEditComponent implements OnInit, OnDestroy {
+  code: Code;
+  private sub: any;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private service: CodeService) { }
 
   ngOnInit() {
+    this.code = new Code();
+    this.sub = this.route.params.subscribe(params => {
+      this.getCode(params['id']);
+    });
+  }
+
+  getCode(id) {
+    this.service.getCode(id).subscribe((c: Code) => {
+      this.code = c;
+      this.code.id = id;
+    });
+  }
+
+  submit() {
+    let _id = this.code.id;
+    this.service.updateCode(this.code).subscribe((m: Result) => {
+      if (m.result == 'success') {
+        alert("Done!");
+        this.router.navigateByUrl('/view/' + _id);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
